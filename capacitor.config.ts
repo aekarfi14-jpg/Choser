8c83a28034a4440710f15fc8ed94e0cd536715a0
aekarfi14-jpg <aekarfi14@gmail.com>
2026-09-03 12:51:43 +0100

    Add Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aekarfi.chooser',
+  appName: 'Choser',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
